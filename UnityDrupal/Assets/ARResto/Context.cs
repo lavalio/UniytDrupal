48555fcc2c1584bc3b78c0341ccee0551947862e
Nian Chun <44611665+lavalio@users.noreply.github.com>
2019-09-11 22:31:21 -0400

    Chang to Fancy ScrollView
    Finally, I found out Fancy ScrollView is usefule

---
+++ UnityDrupal/Assets/ARResto/Context.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace FancyScrollView.MonResto
+{
+    public class Context
+    {
+        public int SelectedIndex = -1;
+        public Action<int> OnCellClicked;
+    }
+}
---
+++ UnityDrupal/Assets/ARResto/MyPage.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+using UnityEngine;
+
+namespace FancyScrollView.MonResto
+{
+    public class MyPage : MonoBehaviour
+    {
+        [SerializeField] ScrollView scrollView = default;
+
+        void Start()
+        {
+            var items = Enumerable.Range(0, 20)
+                .Select(i => new ItemData($"Cell {i}"))
+                .ToArray();
+
+            scrollView.UpdateData(items);
+            scrollView.SelectCell(0);
+        }
+    }
+}
